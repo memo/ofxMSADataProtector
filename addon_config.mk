ce1b8232ea5ec8684a9c7abcd3136a06381db427
Memo Akten <memo@memo.tv>
2019-02-01 04:50:46 +0300

    addon_config

---
+++ addon_config.mk
@@ -0,0 +1,11 @@
+meta:
+	ADDON_NAME = MSADataProtector
+	ADDON_DESCRIPTION = Encrypt files
+	ADDON_AUTHOR = Memo Akten, www.memo.tv
+	ADDON_TAGS = ""
+	ADDON_URL = https://github.com/memo/MSADataProtector
+
+common:
+	# dependencies with other addons, a list of them separated by spaces
+	# or use += in several lines
+	ADDON_DEPENDENCIES =
